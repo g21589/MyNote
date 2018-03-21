e29fd4a77f0cb144fae0319cac67b1821c4f7903
g21589 <g21589@gmail.com>
2018-03-21 22:29:45 +0800

    Update pyspark_multi_df.py

--- pyspark_multi_df.py
+++ pyspark_multi_df.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os, sys, timeit
+import base64
 import numpy as np
 import pandas as pd
 import pyspark
@@ -16,7 +17,7 @@ try:
 except Exception as err:
     logger.warning(err)
     
-#sql = pyspark.SQLContext(sc)
+sql = pyspark.SQLContext(sc)
     
 rdd = sc.parallelize([
     ('A', 1),
@@ -31,12 +32,12 @@ def func(x):
     value = x[1]
     print(value)
     arr1 = [
-            ["Movies" , 46],
-            ["Sports" ,  8],
-            ["Coding" , 12],
-            ["Fishing", 12],
-            ["Dancing",  6],
-            ["cooking",  8]
+            ["Movies" , np.array([1.0, 2.5], dtype=np.float32)],
+            ["Sports" , np.array([1.0, 2.5], dtype=np.float32)],
+            ["Coding" , np.array([1.0, 2.5], dtype=np.float32)],
+            ["Fishing", np.array([1.0, 2.5], dtype=np.float32)],
+            ["Dancing", np.array([1.0, 2.5], dtype=np.float32)],
+            ["cooking", np.array([1.0, 2.5], dtype=np.float32)]
     ]
     arr2 = [
             ["AAA", 6590],
@@ -45,6 +46,9 @@ def func(x):
     
     df1 = pd.DataFrame(arr1, columns = ["name", "num"])
     df2 = pd.DataFrame(arr2, columns = ["name", "num"])
+    
+    #df1['num'] = df1['num'].map(lambda x: bytearray(x))
+    df1['num'] = df1['num'].map(lambda x: base64.b64encode(x).decode('ascii'))
     
     d1 = df1.to_dict(orient='records')
     d2 = df2.to_dict(orient='records')
@@ -87,3 +91,6 @@ df2.show()
 
 timer_end = timeit.default_timer()
 print(timer_end - timer_start)
+
+pdf = df1.toPandas()
+#a = pdf['num'].map(lambda x: np.frombuffer(x, np.float32))
