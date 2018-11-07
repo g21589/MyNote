150ba7454b006d0bab0de1d51b33478e86dc828d
g21589 <g21589@gmail.com>
2018-11-07 23:35:17 +0800

    Add files via upload

--- Train.py
+++ Train.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
+import warnings
 
 import numpy as np
 import pandas as pd
@@ -9,13 +10,15 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
 from keras.applications.inception_v3 import InceptionV3
-from keras.applications.inception_resnet_v2 import InceptionResNetV2
-from keras.applications.nasnet import NASNetLarge
+#from keras.applications.inception_resnet_v2 import InceptionResNetV2
+#from keras.applications.nasnet import NASNetLarge
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.layers import Dense, Dropout, GlobalAveragePooling2D
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras import backend as K
+from keras import layers
+from keras.utils import get_source_inputs
 
 import confusion_matrix_pretty_print
 
@@ -76,11 +79,21 @@ if not os.path.isdir(os.path.dirname(checkpoint_fn)):
 
 #%% ImageDataGenerator
 train_datagen = ImageDataGenerator(
-    rescale=1.0/255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    vertical_flip=True
+    rescale                       = 1.0/255,
+#    featurewise_center            = False,
+#    samplewise_center             = True,
+#    featurewise_std_normalization = False,
+#    samplewise_std_normalization  = True,
+#    zca_whitening                 = False,
+#    zca_epsilon                   = 1e-6,
+#    rotation_range                = 45,
+#    width_shift_range             = 0.0,
+#    height_shift_range            = 0.0,
+#    brightness_range              = [0.1, 0.9],
+    shear_range                   = 0.2,
+    zoom_range                    = 0.2,
+    horizontal_flip               = True,
+    vertical_flip                 = True
 )
 
 train_generator = train_datagen.flow_from_directory(
@@ -99,60 +112,311 @@ valid_generator = valid_datagen.flow_from_directory(
     batch_size=batch_size,
     shuffle=False
 )
+#%% Define custom network
 
-#def generate_generator_multiple(generator, dir1, dir2, batch_size, img_height, img_width):
-#    genX1 = generator.flow_from_directory(dir1,
-#                                          target_size=(img_height,img_width),
-#                                          class_mode='categorical',
-#                                          batch_size=batch_size,
-#                                          shuffle=True,
-#                                          seed=7,
-#                                          #save_to_dir=train_data_gen_dir1,
-#                                          #save_format='png',
-#                                          #save_prefix='gen'
-#                                          )
-#    
-#    genX2 = generator.flow_from_directory(dir2,
-#                                          target_size=(img_height,img_width),
-#                                          class_mode='categorical',
-#                                          batch_size=batch_size,
-#                                          shuffle=True, 
-#                                          seed=7,
-#                                          #save_to_dir=train_data_gen_dir2,
-#                                          #save_format='png',
-#                                          #save_prefix='gen'
-#                                          )
-#    while True:
-#        X1i = genX1.next()
-#        X2i = genX2.next()
-#        X = np.concatenate((X1i[0], X2i[0]), axis=3)
-#        yield X, X2i[1]
-#
-#train_generator = generate_generator_multiple(generator=train_datagen,
-#                                           dir1=train_dir_1,
-#                                           dir2=train_dir_2,
-#                                           batch_size=batch_size,
-#                                           img_height=img_size,
-#                                           img_width=img_size)
+backend = K
+#layers = None
+#models = None
+#keras_utils = None
+
+def _obtain_input_shape(input_shape,
+                        default_size,
+                        min_size,
+                        data_format,
+                        require_flatten,
+                        weights=None):
+    if weights != 'imagenet' and input_shape and len(input_shape) == 3:
+        if data_format == 'channels_first':
+            if input_shape[0] not in {1, 3}:
+                warnings.warn(
+                    'This model usually expects 1 or 3 input channels. '
+                    'However, it was passed an input_shape with ' +
+                    str(input_shape[0]) + ' input channels.')
+            default_shape = (input_shape[0], default_size, default_size)
+        else:
+            if input_shape[-1] not in {1, 3}:
+                warnings.warn(
+                    'This model usually expects 1 or 3 input channels. '
+                    'However, it was passed an input_shape with ' +
+                    str(input_shape[-1]) + ' input channels.')
+            default_shape = (default_size, default_size, input_shape[-1])
+    else:
+        if data_format == 'channels_first':
+            default_shape = (3, default_size, default_size)
+        else:
+            default_shape = (default_size, default_size, 3)
+    if weights == 'imagenet' and require_flatten:
+        if input_shape is not None:
+            if input_shape != default_shape:
+                raise ValueError('When setting`include_top=True` '
+                                 'and loading `imagenet` weights, '
+                                 '`input_shape` should be ' +
+                                 str(default_shape) + '.')
+        return default_shape
+    if input_shape:
+        if data_format == 'channels_first':
+            if input_shape is not None:
+                if len(input_shape) != 3:
+                    raise ValueError(
+                        '`input_shape` must be a tuple of three integers.')
+                if input_shape[0] != 3 and weights == 'imagenet':
+                    raise ValueError('The input must have 3 channels; got '
+                                     '`input_shape=' + str(input_shape) + '`')
+                if ((input_shape[1] is not None and input_shape[1] < min_size) or
+                   (input_shape[2] is not None and input_shape[2] < min_size)):
+                    raise ValueError('Input size must be at least ' +
+                                     str(min_size) + 'x' + str(min_size) +
+                                     '; got `input_shape=' +
+                                     str(input_shape) + '`')
+        else:
+            if input_shape is not None:
+                if len(input_shape) != 3:
+                    raise ValueError(
+                        '`input_shape` must be a tuple of three integers.')
+                if input_shape[-1] != 3 and weights == 'imagenet':
+                    raise ValueError('The input must have 3 channels; got '
+                                     '`input_shape=' + str(input_shape) + '`')
+                if ((input_shape[0] is not None and input_shape[0] < min_size) or
+                   (input_shape[1] is not None and input_shape[1] < min_size)):
+                    raise ValueError('Input size must be at least ' +
+                                     str(min_size) + 'x' + str(min_size) +
+                                     '; got `input_shape=' +
+                                     str(input_shape) + '`')
+    else:
+        if require_flatten:
+            input_shape = default_shape
+        else:
+            if data_format == 'channels_first':
+                input_shape = (3, None, None)
+            else:
+                input_shape = (None, None, 3)
+    if require_flatten:
+        if None in input_shape:
+            raise ValueError('If `include_top` is True, '
+                             'you should specify a static `input_shape`. '
+                             'Got `input_shape=' + str(input_shape) + '`')
+    return input_shape
+
+def conv2d_bn(x,
+              filters,
+              kernel_size,
+              strides=1,
+              padding='same',
+              activation='relu',
+              use_bias=False,
+              name=None):
+    x = layers.Conv2D(filters,
+                      kernel_size,
+                      strides=strides,
+                      padding=padding,
+                      use_bias=use_bias,
+                      name=name)(x)
+    if not use_bias:
+        bn_axis = 1 if backend.image_data_format() == 'channels_first' else 3
+        bn_name = None if name is None else name + '_bn'
+        x = layers.BatchNormalization(axis=bn_axis,
+                                      scale=False,
+                                      name=bn_name)(x)
+    if activation is not None:
+        ac_name = None if name is None else name + '_ac'
+        x = layers.Activation(activation, name=ac_name)(x)
+    return x
+
+
+def inception_resnet_block(x, scale, block_type, block_idx, activation='relu'):
+    if block_type == 'block35':
+        branch_0 = conv2d_bn(x, 32, 1)
+        branch_1 = conv2d_bn(x, 32, 1)
+        branch_1 = conv2d_bn(branch_1, 32, 3)
+        branch_2 = conv2d_bn(x, 32, 1)
+        branch_2 = conv2d_bn(branch_2, 48, 3)
+        branch_2 = conv2d_bn(branch_2, 64, 3)
+        branches = [branch_0, branch_1, branch_2]
+    elif block_type == 'block17':
+        branch_0 = conv2d_bn(x, 192, 1)
+        branch_1 = conv2d_bn(x, 128, 1)
+        branch_1 = conv2d_bn(branch_1, 160, [1, 7])
+        branch_1 = conv2d_bn(branch_1, 192, [7, 1])
+        branches = [branch_0, branch_1]
+    elif block_type == 'block8':
+        branch_0 = conv2d_bn(x, 192, 1)
+        branch_1 = conv2d_bn(x, 192, 1)
+        branch_1 = conv2d_bn(branch_1, 224, [1, 3])
+        branch_1 = conv2d_bn(branch_1, 256, [3, 1])
+        branches = [branch_0, branch_1]
+    else:
+        raise ValueError('Unknown Inception-ResNet block type. '
+                         'Expects "block35", "block17" or "block8", '
+                         'but got: ' + str(block_type))
+
+    block_name = block_type + '_' + str(block_idx)
+    channel_axis = 1 if backend.image_data_format() == 'channels_first' else 3
+    mixed = layers.Concatenate(
+        axis=channel_axis, name=block_name + '_mixed')(branches)
+    up = conv2d_bn(mixed,
+                   backend.int_shape(x)[channel_axis],
+                   1,
+                   activation=None,
+                   use_bias=True,
+                   name=block_name + '_conv')
+
+    x = layers.Lambda(lambda inputs, scale: inputs[0] + inputs[1] * scale,
+                      output_shape=backend.int_shape(x)[1:],
+                      arguments={'scale': scale},
+                      name=block_name)([x, up])
+    if activation is not None:
+        x = layers.Activation(activation, name=block_name + '_ac')(x)
+    return x
+
+
+def InceptionResNetV2(include_top=True,
+                      weights='imagenet',
+                      input_tensor=None,
+                      input_shape=None,
+                      pooling=None,
+                      classes=1000,
+                      **kwargs):
+    #global backend, layers, models, keras_utils
+    #backend, layers, models, keras_utils = get_submodules_from_kwargs(kwargs)
+    
+    if not (weights in {'imagenet', None} or os.path.exists(weights)):
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization), `imagenet` '
+                         '(pre-training on ImageNet), '
+                         'or the path to the weights file to be loaded.')
+
+    if weights == 'imagenet' and include_top and classes != 1000:
+        raise ValueError('If using `weights` as `"imagenet"` with `include_top`'
+                         ' as true, `classes` should be 1000')
+
+    # Determine proper input shape
+    input_shape = _obtain_input_shape(
+        input_shape,
+        default_size=299,
+        min_size=75,
+        data_format=backend.image_data_format(),
+        require_flatten=False,
+        weights=weights)
+
+    if input_tensor is None:
+        img_input = layers.Input(shape=input_shape)
+    else:
+        if not backend.is_keras_tensor(input_tensor):
+            img_input = layers.Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
+    # Stem block: 35 x 35 x 192
+    x = conv2d_bn(img_input, 32, 3, strides=2, padding='valid')
+    x = conv2d_bn(x, 32, 3, padding='valid')
+    x = conv2d_bn(x, 64, 3)
+    x = layers.MaxPooling2D(3, strides=2)(x)
+    x = conv2d_bn(x, 80, 1, padding='valid')
+    x = conv2d_bn(x, 192, 3, padding='valid')
+    x = layers.MaxPooling2D(3, strides=2)(x)
+
+    # Mixed 5b (Inception-A block): 35 x 35 x 320
+    branch_0 = conv2d_bn(x, 96, 1)
+    branch_1 = conv2d_bn(x, 48, 1)
+    branch_1 = conv2d_bn(branch_1, 64, 5)
+    branch_2 = conv2d_bn(x, 64, 1)
+    branch_2 = conv2d_bn(branch_2, 96, 3)
+    branch_2 = conv2d_bn(branch_2, 96, 3)
+    branch_pool = layers.AveragePooling2D(3, strides=1, padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, 64, 1)
+    branches = [branch_0, branch_1, branch_2, branch_pool]
+    channel_axis = 1 if backend.image_data_format() == 'channels_first' else 3
+    x = layers.Concatenate(axis=channel_axis, name='mixed_5b')(branches)
+
+    # 10x block35 (Inception-ResNet-A block): 35 x 35 x 320
+    for block_idx in range(1, 11):
+        x = inception_resnet_block(x,
+                                   scale=0.17,
+                                   block_type='block35',
+                                   block_idx=block_idx)
+
+    # Mixed 6a (Reduction-A block): 17 x 17 x 1088
+    branch_0 = conv2d_bn(x, 384, 3, strides=2, padding='valid')
+    branch_1 = conv2d_bn(x, 256, 1)
+    branch_1 = conv2d_bn(branch_1, 256, 3)
+    branch_1 = conv2d_bn(branch_1, 384, 3, strides=2, padding='valid')
+    branch_pool = layers.MaxPooling2D(3, strides=2, padding='valid')(x)
+    branches = [branch_0, branch_1, branch_pool]
+    x = layers.Concatenate(axis=channel_axis, name='mixed_6a')(branches)
+
+    # 20x block17 (Inception-ResNet-B block): 17 x 17 x 1088
+    for block_idx in range(1, 21):
+        x = inception_resnet_block(x,
+                                   scale=0.1,
+                                   block_type='block17',
+                                   block_idx=block_idx)
+
+    # Mixed 7a (Reduction-B block): 8 x 8 x 2080
+    branch_0 = conv2d_bn(x, 256, 1)
+    branch_0 = conv2d_bn(branch_0, 384, 3, strides=2, padding='valid')
+    branch_1 = conv2d_bn(x, 256, 1)
+    branch_1 = conv2d_bn(branch_1, 288, 3, strides=2, padding='valid')
+    branch_2 = conv2d_bn(x, 256, 1)
+    branch_2 = conv2d_bn(branch_2, 288, 3)
+    branch_2 = conv2d_bn(branch_2, 320, 3, strides=2, padding='valid')
+    branch_pool = layers.MaxPooling2D(3, strides=2, padding='valid')(x)
+    branches = [branch_0, branch_1, branch_2, branch_pool]
+    x = layers.Concatenate(axis=channel_axis, name='mixed_7a')(branches)
+
+    # 10x block8 (Inception-ResNet-C block): 8 x 8 x 2080
+    for block_idx in range(1, 10):
+        x = inception_resnet_block(x,
+                                   scale=0.2,
+                                   block_type='block8',
+                                   block_idx=block_idx)
+    x = inception_resnet_block(x,
+                               scale=1.,
+                               activation=None,
+                               block_type='block8',
+                               block_idx=10)
+
+    # Final convolution block: 8 x 8 x 1536
+    x = conv2d_bn(x, 1536, 1, name='conv_7b')
+
+    if include_top:
+        # Classification block
+        x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+        x = layers.Dense(classes, activation='softmax', name='predictions')(x)
+    else:
+        if pooling == 'avg':
+            x = layers.GlobalAveragePooling2D()(x)
+        elif pooling == 'max':
+            x = layers.GlobalMaxPooling2D()(x)
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    if input_tensor is not None:
+        inputs = get_source_inputs(input_tensor)
+    else:
+        inputs = img_input
+
+    # Create model.
+    model = Model(inputs, x, name='inception_resnet_v2')
+    
+    return model
 
 #%% Define network
 # create the base pre-trained model
-base_model = InceptionV3(weights=None, 
-                         include_top=False,
-                         input_shape=(img_size, img_size, channels))
-#base_model = InceptionResNetV2(weights=None, 
+#base_model = InceptionV3(weights=None, 
 #                         include_top=False,
 #                         input_shape=(img_size, img_size, channels))
+base_model = InceptionResNetV2(weights=None, 
+                         include_top=False,
+                         input_shape=(img_size, img_size, channels))
 #base_model = NASNetLarge(weights=None, 
 #                         include_top=False,
 #                         input_shape=(img_size, img_size, channels))
 
 # add a global spatial average pooling layer
 x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)
-#x = Dropout(0.2)(x)
-predictions = Dense(classes, activation='softmax')(x)
+x = GlobalAveragePooling2D(name='avg_pool')(x)
+x = Dense(1024, activation='relu', name='dense_1024')(x)
+predictions = Dense(classes, activation='softmax', name='predictions')(x)
 
 # this is the model we will train
 model = Model(inputs=base_model.input, outputs=predictions)
@@ -165,9 +429,9 @@ model.compile(
     loss='categorical_crossentropy', 
     metrics=[
         'acc', 
-#        precision, 
-#        recall, 
-#        fmeasure
+        precision, 
+        recall, 
+        fmeasure
     ]
 )
 
@@ -184,11 +448,20 @@ cb_ReduceLR   = ReduceLROnPlateau(monitor='val_acc', factor=0.1, patience=3, ver
 
 #%% Training
 # train the model on the new data for a few epochs
+class_weight = {
+    0: 1.0,
+    1: 1.0,
+    2: 1.0,
+    3: 1.0,
+    4: 1.0
+}
 history = model.fit_generator(
     generator           = train_generator,
     validation_data     = valid_generator,
     steps_per_epoch     = len(train_generator), 
-    epochs              = 3,
+    epochs              = 5,
+    validation_steps    = len(valid_generator),
+#    class_weight        = class_weight,
     max_queue_size      = 16,
     workers             = 4,
     use_multiprocessing = False,
@@ -199,11 +472,11 @@ history = model.fit_generator(
     ]
 )
 
-model.save_weights('Model_NASNetL.h5')
+model.save_weights('Model.h5')
 
 #%% Predict vaildation set
 valid_generator.reset()
-pred_probs = model.predict_generator(valid_generator)
+pred_probs = model.predict_generator(valid_generator, steps=len(valid_generator))
 
 class2index = valid_generator.class_indices
 index2class = dict((v,k) for k,v in class2index.items())
